11101bfc795aea9d209823231d081956edbb7a91
KevinvanLoon <34369608+KevinvanLoon@users.noreply.github.com>
2018-05-28 17:26:27 +0200

    Created Sms object

---
+++ domain/Sms.js
@@ -0,0 +1,9 @@
+class Sms{
+    constructor(sender,reciever,message,token){
+        this.sender = sender;
+        this.reciever = reciever;
+        this.message = message;
+        this.token = token;
+    }
+}
+module.exports = Sms;
