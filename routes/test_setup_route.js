0b5951eb658c9ed50bdb6dbefa1f168f3a216631
Davilicus <florisslenderman@gmail.com>
2018-05-29 12:37:50 +0200

    Added objects in json
    Added the obljects to the status 200 json.

--- routes/test_setup_route.js
+++ routes/test_setup_route.js
@@ -4,10 +4,27 @@ const auth = require('../middleware/auth');
 // Get router
 const router = express.Router();
 
+// Test object
+
+
 // Root post endpoint
 router.post('/', auth, (req, res) => {
 
-    res.status(200).json({message: '', code: 200});
+    res.status(200).json({
+        data: {
+        },
+        samples: {
+            actions: {
+                sendsms: {
+                    sender: 'Kek',
+                    body: 'Kek2',
+                    receiver: 'Kek3',
+                    token: 'Kek4'
+                }
+            }
+        },
+        code: 200
+    });
 });
 
 // Export these endpoints
