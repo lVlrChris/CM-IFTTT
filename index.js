490d6c8e6d25ad99d77ee96c22bb28a1374091f1
Jan Belterman <jjabelte@avans.nl>
2018-05-29 10:36:12 +0200

    Fixes bugs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const status = require('./routes/status');
 const app = express();
 app.use(express.json());
 app.use('/api/ifttt/v1/actions/sms', sms);
-app.use('/api/ifttt/v1/actions/status', status);
+app.use('/api/ifttt/v1/status', status);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
