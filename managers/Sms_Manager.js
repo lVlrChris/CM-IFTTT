c6eee34baa4c4f593ea42c932dc310307053dfa9
Jan Belterman <jjabelte@avans.nl>
2018-05-29 09:56:58 +0200

    Send sms manager & endpoint

---
+++ managers/Sms_Manager.js
@@ -0,0 +1,54 @@
+const request = require('request');
+
+// TODO: create all responses
+module.exports = {
+    sendSms(req, res) {
+
+        // Get input from ifttt
+        const iftttInput = {
+            sender: req.body.actionFields.sender,
+            body: req.body.actionFields.body,
+            receiver: req.body.actionFields.receiver,
+            token: req.body.actionFields.token
+        };
+        console.log('Content from CM\n', iftttInput);
+
+        // TODO: validate input
+        // - Sender and body required
+        // - Sender max 11 characters
+
+        // TODO: delegate responsibility to other module
+        // convert ifttt input to CM SMS
+        const cmSMS = {
+            messages: {
+                authentication: {
+                    producttoken: iftttInput.token
+                },
+                msg: [{
+                    from: iftttInput.sender,
+                    to: [{
+                        number: iftttInput.receiver
+                    }],
+                    body: {
+                        content: iftttInput.body
+                    }
+                }]
+            }
+        };
+
+        // TODO: delegate sending responsibility to manager
+        // Send request to CM
+        request({
+            url: "https://gw.cmtelecom.com/v1.0/message",
+            method: "POST",
+            json: true,
+            body: cmSMS
+        }, function (error, response, body){
+            if (error) console.log(error);
+            else console.log(body);
+        });
+        // Return response
+        res.status(200).send('SMS send');
+
+    },
+};
--- routes/sms.js
+++ routes/sms.js
@@ -1,11 +1,9 @@
 const express = require('express');
+const { sendSms } = require('../managers/Sms_Manager');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', sendSms);
 
-
-
-});
-
+// Export these endpoints
 module.exports = router;
