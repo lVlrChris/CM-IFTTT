3dc547ecf24b2cdd9f97c673f2d0dd7ce74977d3
Jan Belterman <jjabelte@avans.nl>
2018-05-29 10:47:30 +0200

    Fixed route naming
    IFTTT requires an endpoint name of at least 4 characters (sms -> sendsms).

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const status = require('./routes/status_route');
 
 const app = express();
 app.use(express.json());
-app.use('/api/ifttt/v1/actions/sms', sms);
+app.use('/api/ifttt/v1/actions/sendsms', sms);
 app.use('/api/ifttt/v1/status', status);
 
 const port = process.env.PORT || 3000;
