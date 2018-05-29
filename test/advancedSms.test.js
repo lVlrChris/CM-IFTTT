646daa45646f721c5d5419d53102d01cba9accc1
Rudwan <rudwanakhiat1997@gmail.com>
2018-05-30 01:58:10 +0200

    improved test specifications
    Added things for future tests

--- test/advancedSms.test.js
+++ test/advancedSms.test.js
@@ -1,5 +1,10 @@
 var chai = require('chai'), chaiHttp = require('chai-http');
 chai.use(chaiHttp);
+chai.use(require('chai-things'));
+
+const assertArrays = require('chai-arrays');
+chai.use(assertArrays)
+
 var expect = require('chai').expect;
 var app = 'https://gw.cmtelecom.com';
 
@@ -10,21 +15,6 @@ var jim = '0031630273638'
 describe("advanced tests", function() {
     describe("invalid receiver", function(){
         it("errorCode 201, messageErrorCode 303", function (done){
-            // this.timeout(10000);
-            const expectedResult = {
-                "details": "Created 0 message(s)",
-                "errorCode": 201,
-                "messages": [
-                    {
-                        "to": "invalid",
-                        "status": "Rejected",
-                        "reference": null,
-                        "parts": 0,
-                        "messageDetails": "Gsm 'invalid' is not a number.",
-                        "messageErrorCode": 303
-                    }
-                ]
-            };
             chai.request(app)
                 .post('/v1.0/message')
                 .send({
@@ -45,8 +35,11 @@ describe("advanced tests", function() {
                     }
                 })
                 .end(function (err, res) {
-                    console.log(res.body)
-                    expect(res.body).to.eql(expectedResult);
+                    console.log(res.body.errorCode);
+                    console.log(res.body.messages[0].messageErrorCode);
+
+                    expect(res.body.errorCode).to.equal(201);
+                    expect(res.body.messages[0].messageErrorCode).to.equal(303);
                     done();
                 });
         })
