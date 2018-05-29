28eade2a9e4b698460211720c88a069a79626f28
Davilicus <florisslenderman@gmail.com>
2018-05-29 12:13:01 +0200

    More work for test/setup endpoint
    Added a different item to status 200 result

--- routes/test_setup_route.js
+++ routes/test_setup_route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const auth = require('../middleware/auth');
 
+// Get router
 const router = express.Router();
 
+// Root post endpoint
 router.post('/', auth, (req, res) => {
 
-
-    res.status(200).send('Valid key');
+    res.status(200).json({message: '', code: 200});
 });
 
 // Export these endpoints
