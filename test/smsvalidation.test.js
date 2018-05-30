455151e7d9de7e6cdec2c1d848b2b9a24308c0a1
Kevin <kloon3@avans.nl>
2018-05-30 21:06:02 +0200

    Worked on token tests

--- test/smsvalidation.test.js
+++ test/smsvalidation.test.js
@@ -4,7 +4,7 @@ const server = require('../index')
 
 //Test constants
 const validIftttKey = '12345';
-const invalidPhonenumber = '0031612345678'
+const fakePhoneNumber = '0031612345678'
 const phoneNumberKevin = '0031639023866'
 
 chai.should();
@@ -13,7 +13,6 @@ chai.use(chaiHttp);
 
 //TODO: Discuss if we have to make testcases for using special characters in a request
 describe('Validation of sender',()=>{
-
     it('should throw an error when using an int as sender', (done)=> {
         chai.request(server)
             .post('/api/ifttt/v1/actions/sendsms')
@@ -22,7 +21,7 @@ describe('Validation of sender',()=>{
                "actionFields" : {
                    "sender" : 1234,
                    "body" : "testBody",
-                   "receiver" : invalidPhonenumber,
+                   "receiver" : fakePhoneNumber,
                    "token" : "939DA045-26F7-461F-90FF-C41969F81057"
                }
             })
@@ -35,7 +34,6 @@ describe('Validation of sender',()=>{
                 done();
             });
     });
-
     it('should NOT throw an error when using an string smaller than 16 digits', (done)=> {
         chai.request(server)
             .post('/api/ifttt/v1/actions/sendsms')
@@ -44,7 +42,7 @@ describe('Validation of sender',()=>{
                 "actionFields" : {
                     "sender" : '123',
                     "body" : "testBody",
-                    "receiver" : invalidPhonenumber,
+                    "receiver" : fakePhoneNumber,
                     "token" : "939DA045-26F7-461F-90FF-C41969F81057"
                 }
             })
@@ -62,7 +60,7 @@ describe('Validation of sender',()=>{
                 "actionFields" : {
                     "sender" : 'abcdefghijklmnopqrstuvwx',
                     "body" : "testBody",
-                    "receiver" : invalidPhonenumber,
+                    "receiver" : fakePhoneNumber,
                     "token" : "939DA045-26F7-461F-90FF-C41969F81057"
                 }
             })
@@ -80,7 +78,7 @@ describe('Validation of sender',()=>{
                 "actionFields" : {
                     "sender" : '1234567890123456',
                     "body" : "testBody",
-                    "receiver" : invalidPhonenumber,
+                    "receiver" : fakePhoneNumber,
                     "token" : "939DA045-26F7-461F-90FF-C41969F81057"
                 }
             })
@@ -98,7 +96,7 @@ describe('Validation of sender',()=>{
                 "actionFields" : {
                     "sender" : '1234567890123456789',
                     "body" : "testBody",
-                    "receiver" : invalidPhonenumber,
+                    "receiver" : fakePhoneNumber,
                     "token" : "939DA045-26F7-461F-90FF-C41969F81057"
                 }
             })
@@ -116,7 +114,7 @@ describe('Validation of sender',()=>{
                 "actionFields" : {
                     "sender" : 'abc123abc12',
                     "body" : "testBody",
-                    "receiver" : invalidPhonenumber,
+                    "receiver" : fakePhoneNumber,
                     "token" : "939DA045-26F7-461F-90FF-C41969F81057"
                 }
             })
@@ -134,7 +132,7 @@ describe('Validation of sender',()=>{
                 "actionFields" : {
                     "sender" : 'abc123abc1234567',
                     "body" : "testBody",
-                    "receiver" : invalidPhonenumber,
+                    "receiver" : fakePhoneNumber,
                     "token" : "939DA045-26F7-461F-90FF-C41969F81057"
                 }
             })
@@ -144,7 +142,6 @@ describe('Validation of sender',()=>{
                 done();
             });
     });
-
     it('should throw an error when not providing a sender', (done)=> {
         chai.request(server)
             .post('/api/ifttt/v1/actions/sendsms')
@@ -152,7 +149,7 @@ describe('Validation of sender',()=>{
             .send({
                 "actionFields" : {
                     "body" : "testBody",
-                    "receiver" : invalidPhonenumber,
+                    "receiver" : fakePhoneNumber,
                     "token" : "939DA045-26F7-461F-90FF-C41969F81057"
                 }
             })
@@ -182,16 +179,84 @@ describe('Validation of receiver',()=>{
 
 describe('Validation of token',()=>{
     it('should throw an error when using an int as token', (done)=> {
-
+        chai.request(server)
+            .post('/api/ifttt/v1/actions/sendsms')
+            .set('IFTTT-Service-Key', validIftttKey)
+            .send({
+                "actionFields" : {
+                    "sender" : 'sender',
+                    "body" : "testBody",
+                    "receiver" : fakePhoneNumber,
+                    "token" : 0
+                }
+            })
+            .end(function (err, res) {
+                res.should.have.status(412);
+                res.should.be.json;
+                res.body.should.have.property('message');
+                res.body.should.have.property('datetime');
+                res.body.should.have.property('errorCode');
+                done();
+            });
     });
-    it('should throw an error when using an invalid token', (done)=> {
-
-    });
+   // it('should throw an error when using an invalid token', (done)=> {
+        // chai.request(server)
+        //     .post('/api/ifttt/v1/actions/sendsms')
+        //     .set('IFTTT-Service-Key', validIftttKey)
+        //     .send({
+        //         "actionFields" : {
+        //             "sender" : 'test',
+        //             "body" : "testBody",
+        //             "receiver" : invalidPhonenumber,
+        //             "token" : "12345678-12345678-12344567-12344566-123323445"
+        //         }
+        //     })
+        //     .end(function (err, res) {
+        //         res.should.have.status(412);
+        //         res.should.be.json;
+        //         res.body.should.have.property('message');
+        //         res.body.should.have.property('datetime');
+        //         res.body.should.have.property('errorCode');
+        //         done();
+        //     });
+   // });
     it('should respond status 200 when using a valid token', (done)=> {
-
+        chai.request(server)
+            .post('/api/ifttt/v1/actions/sendsms')
+            .set('IFTTT-Service-Key', validIftttKey)
+            .send({
+                "actionFields" : {
+                    "sender" : 'sender',
+                    "body" : "testBody",
+                    "receiver" : fakePhoneNumber,
+                    "token" : '939DA045-26F7-461F-90FF-C41969F81057'
+                }
+            })
+            .end(function (err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                done();
+            });
     });
     it('should throw an error when not providing a token', (done)=> {
-
+        chai.request(server)
+            .post('/api/ifttt/v1/actions/sendsms')
+            .set('IFTTT-Service-Key', validIftttKey)
+            .send({
+                "actionFields" : {
+                    "sender" : 'sender',
+                    "body" : "testBody",
+                    "receiver" : fakePhoneNumber,
+                }
+            })
+            .end(function (err, res) {
+                res.should.have.status(412);
+                res.should.be.json;
+                res.body.should.have.property('message');
+                res.body.should.have.property('datetime');
+                res.body.should.have.property('errorCode');
+                done();
+            });
     });
 });
 
