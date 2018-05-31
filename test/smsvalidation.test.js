cffde1ac3b59736856311d1c4cc0e064a25804d2
Unknown <kloon3@avans.nl>
2018-05-31 20:26:08 +0200

    Added token,body tests

--- test/smsvalidation.test.js
+++ test/smsvalidation.test.js
@@ -6,6 +6,7 @@ const server = require('../index');
 const validIftttKey = '12345';
 const fakePhoneNumber = '0031612345678';
 const phoneNumberKevin = '0031639023866';
+const longString160Chars = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
 
 chai.should();
 chai.use(chaiHttp);
@@ -105,7 +106,7 @@ describe('Validation of sender',()=>{
                 done();
             });
     });
-    it('should respond status 200 when more than 16 digits', (done)=> {
+    it('should respond status 200 when using more than 16 digits', (done)=> {
         chai.request(server)
             .post('/api/ifttt/v1/actions/sendsms')
             .set('IFTTT-Service-Key', validIftttKey)
@@ -149,7 +150,7 @@ describe('Validation of sender',()=>{
                 done();
             });
     });
-    it('should respond status 200 when using more than11 alphanumeric characters', (done)=> {
+    it('should respond status 200 when using more than 11 alphanumeric characters', (done)=> {
         chai.request(server)
             .post('/api/ifttt/v1/actions/sendsms')
             .set('IFTTT-Service-Key', validIftttKey)
@@ -200,13 +201,75 @@ describe('Validation of sender',()=>{
 //TODO: Discuss how to validate a mobile phone number
 describe('Validation of receiver',()=>{
     it('should throw an error when using an int as receiver', (done)=> {
-
+        chai.request(server)
+            .post('/api/ifttt/v1/actions/sendsms')
+            .set('IFTTT-Service-Key', validIftttKey)
+            .send({
+                "actionFields" : {
+                    "sender" : 'test',
+                    "body" : "testBody",
+                    "receiver" : 0,
+                    "token" : "939DA045-26F7-461F-90FF-C41969F81057"
+                },
+                "ifttt_source" : {
+                    "id" : "test",
+                    "url" : "test"
+                }
+            })
+            .end(function (err, res) {
+                res.should.have.status(412);
+                res.body.should.have.property('message');
+                res.body.should.have.property('datetime');
+                res.body.should.have.property('errorCode');
+                res.should.be.json;
+                done();
+            });
     });
     it('should respond status 200 when using a international format number string', (done)=> {
-
+        chai.request(server)
+            .post('/api/ifttt/v1/actions/sendsms')
+            .set('IFTTT-Service-Key', validIftttKey)
+            .send({
+                "actionFields" : {
+                    "sender" : '1234567890123456789',
+                    "body" : "testBody",
+                    "receiver" : "0031612345678",
+                    "token" : "939DA045-26F7-461F-90FF-C41969F81057"
+                },
+                "ifttt_source" : {
+                    "id" : "test",
+                    "url" : "test"
+                }
+            })
+            .end(function (err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                done();
+            });
     });
-    it('should throw an error when not providing a sender', (done)=> {
-
+    it('should throw an error when not providing a receiver', (done)=> {
+        chai.request(server)
+            .post('/api/ifttt/v1/actions/sendsms')
+            .set('IFTTT-Service-Key', validIftttKey)
+            .send({
+                "actionFields" : {
+                    "sender" : '1234567890123456789',
+                    "body" : "testBody",
+                    "token" : "939DA045-26F7-461F-90FF-C41969F81057"
+                },
+                "ifttt_source" : {
+                    "id" : "test",
+                    "url" : "test"
+                }
+            })
+            .end(function (err, res) {
+                res.should.have.status(412);
+                res.body.should.have.property('message');
+                res.body.should.have.property('datetime');
+                res.body.should.have.property('errorCode');
+                res.should.be.json;
+                done();
+            });
     });
 });
 
@@ -236,31 +299,6 @@ describe('Validation of token',()=>{
                 done();
             });
     });
-   // it('should throw an error when using an invalid token', (done)=> {
-        // chai.request(server)
-        //     .post('/api/ifttt/v1/actions/sendsms')
-        //     .set('IFTTT-Service-Key', validIftttKey)
-        //     .send({
-        //         "actionFields" : {
-        //             "sender" : 'test',
-        //             "body" : "testBody",
-        //             "receiver" : invalidPhonenumber,
-        //             "token" : "12345678-12345678-12344567-12344566-123323445"
-        //         },
-    //                 "ifttt_source" : {
-    //                     "id" : "test",
-    //                     "url" : "test"
-    //                 }
-        //     })
-        //     .end(function (err, res) {
-        //         res.should.have.status(412);
-        //         res.should.be.json;
-        //         res.body.should.have.property('message');
-        //         res.body.should.have.property('datetime');
-        //         res.body.should.have.property('errorCode');
-        //         done();
-        //     });
-   // });
     it('should respond status 200 when using a valid token', (done)=> {
         chai.request(server)
             .post('/api/ifttt/v1/actions/sendsms')
@@ -279,7 +317,6 @@ describe('Validation of token',()=>{
             })
             .end(function (err, res) {
                 res.should.have.status(200);
-                res.should.be.json;
                 done();
             });
     });
@@ -310,16 +347,87 @@ describe('Validation of token',()=>{
 });
 
 describe('Validation of content',()=>{
-    it('should throw an error when using an int as content', (done)=> {
-
+    it('should throw an error when using an int as body', (done)=> {
+        chai.request(server)
+            .post('/api/ifttt/v1/actions/sendsms')
+            .set('IFTTT-Service-Key', validIftttKey)
+            .send({
+                "actionFields" : {
+                    "sender" : 'sender',
+                    "body" : 0,
+                    "receiver" : fakePhoneNumber,
+                    "token" : '939DA045-26F7-461F-90FF-C41969F81057'
+                },
+                "ifttt_source" : {
+                    "id" : "test",
+                    "url" : "test"
+                }
+            })
+            .end(function (err, res) {
+                res.should.have.status(412);
+                done();
+            });
     });
-    it('should throw an error when using a content longer than 160chars', (done)=> {
-
+    it('should throw an error when using a body longer than 160chars', (done)=> {
+        chai.request(server)
+            .post('/api/ifttt/v1/actions/sendsms')
+            .set('IFTTT-Service-Key', validIftttKey)
+            .send({
+                "actionFields" : {
+                    "sender" : 'sender',
+                    "body" : longString160Chars,
+                    "receiver" : fakePhoneNumber,
+                    "token" : '939DA045-26F7-461F-90FF-C41969F81057'
+                },
+                "ifttt_source" : {
+                    "id" : "test",
+                    "url" : "test"
+                }
+            })
+            .end(function (err, res) {
+                res.should.have.status(412);
+                done();
+            });
     });
-    it('should respond status 200 when using a string content smaller than 160 chars', (done)=> {
-
+    it('should respond status 200 when using a string body smaller than 160 chars', (done)=> {
+        chai.request(server)
+            .post('/api/ifttt/v1/actions/sendsms')
+            .set('IFTTT-Service-Key', validIftttKey)
+            .send({
+                "actionFields" : {
+                    "sender" : 'sender',
+                    "body" : "uno",
+                    "receiver" : fakePhoneNumber,
+                    "token" : '939DA045-26F7-461F-90FF-C41969F81057'
+                },
+                "ifttt_source" : {
+                    "id" : "test",
+                    "url" : "test"
+                }
+            })
+            .end(function (err, res) {
+                res.should.have.status(200);
+                done();
+            });
     });
-    it('should throw an error when not providing a content', (done)=> {
-
+    it('should throw an error when not providing a body', (done)=> {
+        chai.request(server)
+            .post('/api/ifttt/v1/actions/sendsms')
+            .set('IFTTT-Service-Key', validIftttKey)
+            .send({
+                "actionFields" : {
+                    "sender" : 'sender',
+                    "receiver" : fakePhoneNumber,
+                    "token" : '939DA045-26F7-461F-90FF-C41969F81057'
+                },
+                "ifttt_source" : {
+                    "id" : "test",
+                    "url" : "test"
+                }
+            })
+            .end(function (err, res) {
+                res.should.have.status(412);
+                done();
+            });
     });
 });
