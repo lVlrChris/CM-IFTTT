29ec667f90514f6bdc0ce0a3dbd0d2c24d60656d
Floris Botermans <fboterma@avans.nl>
2018-06-06 11:28:53 +0200

    Fixed voice validation

--- domain/Voice.js
+++ domain/Voice.js
@@ -2,29 +2,23 @@ const joi = require('joi');
 const ApiError = require('../domain/ApiError');
 
 //Validate function for a voice object
-function validate(callee, caller, anonymous, prompt, promptType, language, gender, number){
+function validate(receiver, sender, body, language, token){
     //Voice object, used for checking if the object matches the schema
     const voiceObject = {
-        callee : callee,
-        caller: caller,
-        anonymous: anonymous,
-        prompt: prompt,
-        promptType: promptType,
+        receiver: receiver,
+        sender: sender,
+        body: body,
         language: language,
-        gender: gender,
-        number: number
+        token: token
     };
 
     //Schema for a voice message, this defines what a voice message should look like
     const schema = {
-        callee : joi.string().required(),
-        caller: joi.string().required(),
-        anonymous: joi.string().required(),
-        prompt: joi.string().required(),
-        promptType: joi.string().required(),
+        receiver: joi.string().required(),
+        sender: joi.string().required(),
+        body: joi.string().max(160).required(),
         language: joi.string().required(),
-        gender: joi.string().required(),
-        number: joi.string().numeric().required()
+        token: joi.string().required()
     };
 
     //Validate voice message and return result
