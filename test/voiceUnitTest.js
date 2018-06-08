2cadd8f28cdf3cbe4283a2e30c28d3361bdc058c
Chris <christian-boer@hotmail.com>
2018-06-08 10:49:00 +0200

    WIP 3

--- test/voiceUnitTest.js
+++ test/voiceUnitTest.js
@@ -3,8 +3,10 @@ const chaiHttp = require('chai-http');
 const server = require('../index');
 
 //Test constants
-const validIftttKey = '12345';
+const localIftttKey = '12345';
 const fakePhoneNumber = '0031612345678';
+const fakeCMToken = '1t2o3k4e5n';
+const fakeCMSharedKey = '1s2h3a4r5e6d7k8e9y';
 
 //call should function for starting point
 chai.should();
@@ -13,6 +15,62 @@ chai.should();
 chai.use(chaiHttp);
 
 //Authentication tests?
+
+describe('Validation of the actionFields key', () => {
+    it('should throw an error when the actionFields key is not provided', (done) => {
+        chai.request(server)
+            .post('/api/ifttt/v1/actions/send_voice_message')
+            .set('IFTTT-Service-Key', localIftttKey)
+            .send({
+                "ifttt_source": {
+                    "id": "2",
+                    "url": "https://ifttt.com/myrecipes/personal/2"
+                },
+                "user": {
+                    "timezone": "Pacific Time (US & Canada)"
+                }
+            })
+            .end(function (err, res) {
+                res.should.have.status(412);
+                res.should.be.json;
+                res.body.should.have.property('message');
+                res.body.should.have.property('datetime');
+                res.body.should.have.property('errorCode');
+                done();
+            });
+    });
+
+    it('should not throw an error when actionFields is provided', (done) => {
+        chai.request(server)
+            .post('/api/ifttt/v1/actions/send_voice_message')
+            .set('IFTTT-Service-Key', localIftttKey)
+            .send({
+                "actionFields": {
+                    "sender": "0031687654321",
+                    "receiver": fakePhoneNumber,
+                    "body": "This is a sample message",
+                    "language": "nl",
+                    "token": fakeCMToken,
+                    "username": "sampleUsername",
+                    "sharedkey": fakeCMSharedKey
+                },
+                "ifttt_source": {
+                    "id": "test",
+                    "url": "test"
+                },
+                "user": {
+                    "timezone": "Pacific Time (US & Canada)"
+                }
+            })
+            .end(function (err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.have.property('data');
+                res.body.data.should.have.property('id');
+                res.body.data.should.have.property('url');
+            })
+    })
+});
 
 //Tests for the input from a voice action
 describe('Validation of instruction id', () => {
