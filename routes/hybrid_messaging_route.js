4f1e9a76173cad26da05aff4fe1026d20acb3066
Kevin <kloon3@avans.nl>
2018-06-13 10:33:40 +0200

    Updated hybrid route

--- routes/hybrid_messaging_route.js
+++ routes/hybrid_messaging_route.js
@@ -1,13 +1,13 @@
 // Require express and sms manager
 const express = require('express');
-// const { hybrid_message } = require('../managers/hybrid_message_manager');
+ const { sendHybrid } = require('../managers/hybrid_manager');
 const auth = require('../middleware/auth');
 
 // Get router
 const router = express.Router();
 
 // Configure root post endpoint
-// router.post('/', auth, hybrid_message);
+ router.post('/', auth, sendHybrid);
 
 // Export these endpoints
 module.exports = router;
