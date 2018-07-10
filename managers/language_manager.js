c0ddd5306308d21ce12b507339e696f7cd066859
Chris <christian-boer@hotmail.com>
2018-07-10 17:49:29 +0200

    Language api connection done

--- managers/language_manager.js
+++ managers/language_manager.js
@@ -13,32 +13,47 @@ module.exports = {
         // }
 
         //Get language list from CM endpoint
-        let cmResponse;
-
-        request({
-            url: "https://api.cmtelecom.com/voicesendapi/v1.0/tts/languages",
-            method: "POST",
-            json: true
-        }, function (error, response, body) {
-            if (error) {
-                console.log(error);
-            } else {
-                cmResponse = body;
+        const cmResponse = new Promise(
+            function (resolve, reject) {
+                request({
+                    url: "https://api.cmtelecom.com/voicesendapi/v1.0/tts/languages",
+                    method: "GET",
+                    json: true
+                }, function (error, response, body) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        resolve(body);
+                    }
+                });
             }
+        ).then(function (result) {
+            //Format list for IFTTT
+
+            let languages = {data: []};
+
+            const resultSize = Object.keys(result).length;
+
+            for (let i = 0; i < resultSize; i++) {
+                const key = Object.keys(result)[i];
+                const value = result[key];
+
+                const langObject = {value: key, label: value};
+
+                languages.data.push(langObject);
+            }
+
+            console.log(languages);
+
+            //Respond with the language list
+
+            if (languages.data.size <= 0) {
+                res.status(400).send({error: "Could not find languages"});
+            } else {
+                res.status(200).send(languages);
+            }
+
         });
-
-        let languages = {data: []};
-
-        for (let i = 0; i < cmResponse.size; i++) {
-            const key = Object.keys(cmResponse[i]);
-            const value = cmResponse[i];
-
-            const langObject = {value: key, label: value};
-
-            langObject.data.push(langObject);
-        }
-
-        console.log(languages);
 
     }
 };
