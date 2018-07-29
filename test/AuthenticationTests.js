7591f473101626522dc97169923894d4613b6d64
Kevin <kloon3@avans.nl>
2018-07-29 17:43:44 +0200

    Created authentication tests file

---
+++ test/AuthenticationTests.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../index');
+
+//Using the chai package to call .should on responses
+chai.should();
+
+//Using chaiHttp to make http request to our api
+chai.use(chaiHttp);
+
