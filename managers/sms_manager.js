6345ee78d8c01da177461af0c0dc3fc5bb49a993
Kevin <kloon3@avans.nl>
2018-08-06 17:24:46 +0200

    created todo and wip

--- managers/sms_manager.js
+++ managers/sms_manager.js
@@ -124,8 +124,10 @@ module.exports = {
                 res.status(200).send(response);
             })
             .catch((err)=>{
+
+                //TODO: Een generieke token nodig om door de auth van cm te komen
                 console.log(smsObject.token);
-                if (smsObject.token === '0000000-0000-0000-0000-000000000000'){
+                if (smsObject.token === '0000000-0000-0000-0000-000000000011100'){
                     console.log("Creating responses for IFTTT");
                     // Create a response with the request id and url from IFTTT.
                     let response;
