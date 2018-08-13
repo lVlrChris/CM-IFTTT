4296d5748d41acc1797ae068ed199136a06b5aa8
Kevin <kloon3@avans.nl>
2018-08-13 09:54:18 +0200

    removed deprecated functio in contact manager

--- managers/addContact_manager.js
+++ managers/addContact_manager.js
@@ -140,42 +140,6 @@ function createContact(contact, accountID, groupID, token, res, req, next) {
 
 }
 
-function createResponsesForIFTTT(requestBody) {
-
-    console.log("Creating responses for IFTTT");
-    // Create a response with the request id and url from IFTTT.
-    let response;
-    if (!requestBody.ifttt_source) {
-        console.log("No source");
-        return {
-            "data": [
-                {
-                    "id": "no id"
-                }
-            ]
-        };
-    } else {
-        if (typeof requestBody.ifttt_source.id !== 'undefined' && typeof requestBody.ifttt_source.url !== 'undefined') {
-            return {
-                "data": [
-                    {
-                        "id": requestBody.ifttt_source.id,
-                        "url": requestBody.ifttt_source.url
-                    }
-                ]
-            };
-        } else if (typeof requestBody.ifttt_source.id !== 'undefined') {
-            return {
-                "data": [
-                    {
-                        "id": "no id"
-                    }
-                ]
-            };
-        }
-    }
-
-}
 
 module.exports = {
     async addContact(req, res, next) {
