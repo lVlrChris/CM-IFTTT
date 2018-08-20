b2b7dda79073a23fb10b2fa5e328f714f86938f3
Kevin <kloon3@avans.nl>
2018-08-20 14:30:09 +0200

    WIP inbound manager

---
+++ domain/InboundSms.js
@@ -0,0 +1,32 @@
+const joi = require('joi');
+const ApiError = require('../domain/ApiError');
+class InboundSms {
+
+    constructor(receiver){
+
+        const { error } = validate(receiver);
+        if(error) throw error;
+
+        this.receiver = receiver;
+    }catch (e) {
+        throw (new ApiError(e.details[0].message, 400));
+    }
+
+
+
+}
+
+//Validate function for a voice object
+function validate(receiver){
+    //Voice object, used for checking if the object matches the schema
+    const inboundObject = {
+        receiver: receiver,
+    };
+
+    const schema = {
+        receiver: joi.string().required(),
+    };
+
+    //Validate voice message and return result
+    return joi.validate(inboundObject,schema);
+}
--- managers/inbound_sms_manager.js
+++ managers/inbound_sms_manager.js
@@ -1,7 +1,33 @@
-
+const InboundSms = require("../domain/InboundSms");
+const ApiError = require('../domain/ApiError');
+const rp = require('request-promise');
+const IFTTTFormatter = require('../domain/IFTTTFormatter');
 
 module.exports = {
     getInboundSms(req, res, next) {
+
+
+        let receiver = null;
+        let specifiedSender = null;
+
+        if (typeof req.body.actionFields !== 'undefined') {
+
+            specifiedSender = req.body.actionFields.specified_sender || "";
+            receiver = req.body.actionFields.receiver || "";
+
+        } else {
+            next(new ApiError('actionFields key not provided.', 400));
+            return;
+        }
+
+        let inboundObject = null;
+
+        try {
+            inboundObject = new InboundSms(receiver);
+        } catch (apiError) {
+            next(apiError);
+            return;
+        }
         res.sendStatus(200);
     }
 };
