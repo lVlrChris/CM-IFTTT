9bf91120ded94942eb1c65c1972cd91b416fb255
Chris <christian-boer@hotmail.com>
2018-08-20 16:13:08 +0200

    Database manager

--- config/config.js
+++ config/config.js
@@ -4,11 +4,13 @@ const envNotifireAppkey = process.env.APPKEY_NOTIFIRE;
 const envOAuth2ClientID = process.env.OAUTH2_CLIENT_ID;
 const envOAuth2ClientSecret = process.env.OAUTH2_CLIENT_SECRET;
 const tempToken = process.env.OAUTH2_TEMP_TOKEN;
+const dbHostAddr = process.env.PGHOSTADDR;
 
 module.exports = {
     iftttServiceKey: envIftttServiceKey,
     notifireAppkey: envNotifireAppkey,
     oAuthID: envOAuth2ClientID,
     oAuthSecret: envOAuth2ClientSecret,
-    oAuthTToken: tempToken
+    oAuthTToken: tempToken,
+    dbHostAddr: dbHostAddr
 };
