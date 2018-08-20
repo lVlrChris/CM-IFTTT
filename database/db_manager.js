4b72248319a9671527f2964d1f61e2bd526bc860
Chris <christian-boer@hotmail.com>
2018-08-20 17:37:45 +0200

    New db manager

---
+++ database/db_manager.js
@@ -0,0 +1,18 @@
+const db = require('./db_connection');
+
+module.exports = {
+    //Create inbound sms entry
+    createInboundSms(inboundReplySms) {
+        
+    },
+
+    //Get all inbound sms entries (from a certain user account)
+    getInboundSms() {
+
+    },
+
+    //Get inbound sms entries from a specified sender (from a certain user account)
+    getInboundSms(sender) {
+
+    }
+};
