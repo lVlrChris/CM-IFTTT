5de179604cda02a344f6d88592b0f6dfa888e843
Chris <christian-boer@hotmail.com>
2018-08-22 16:40:50 +0200

    Create reply sms entry

--- database/db_manager.js
+++ database/db_manager.js
@@ -3,7 +3,23 @@ const db = require('./db_connection');
 module.exports = {
     //Create inbound sms entry
     createInboundSms(inboundReplySms) {
-        db.query('INSERT INTO inbound_reply_sms (replyid, receiver, sender, message, reference, productkey) VALUES (\'$1\')
+
+        const queryText = `INSERT INTO inbound_reply_sms (replyid, receiver, sender, message, reference, productkey) 
+        VALUES ($1, $2, $3, $4, $5, $6) RETURNING inbound_reply_sms.id`;
+        const values = [inboundReplySms.replyId,
+        inboundReplySms.receiver,
+        inboundReplySms.sender,
+        inboundReplySms.message,
+        inboundReplySms.reference,
+        inboundReplySms.productKey];
+
+        db.query(queryText, values, (err, res) => {
+            if(err) {
+                console.log(err.stack);
+            } else {
+                console.log(res.rows);
+            }
+        });
     },
 
     //Get all inbound sms entries (from a certain user account)
